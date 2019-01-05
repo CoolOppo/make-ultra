0fe5c59cea270765e31c9219e1b4b13062103259
CoolOppo <CoolOppo@users.noreply.github.com>
2019-01-05 07:58:24 -0500

    saving progress

---
+++ src/file.rs
@@ -0,0 +1,17 @@
+use std::path::Path;
+use crate::rule;
+use std::process::Command;
+#[derive(Debug)]
+pub struct File<'a> {
+	pub path: &'a Path,
+	pub rule: &'a rule::Rule
+}
+
+impl File<'_> {
+	pub fn run_commands(&self) {
+		Command::new("cmd")
+                    .args(&["/C", &self.rule.command])
+                    .output()
+                    .expect("failed to execute process");
+	}
+}
