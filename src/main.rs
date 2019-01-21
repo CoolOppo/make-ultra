4d2affa1adeba47f22b0e2653c1f54ff30d88751
CoolOppo <CoolOppo@users.noreply.github.com>
2019-01-21 09:22:08 -0500

    fully working now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all)]
-#![allow(dead_code, unused_variables)]
+#![allow(dead_code, unused_variables, unused_assignments, unused_imports)]
 
+extern crate clap;
 extern crate ignore;
 #[macro_use]
 extern crate lazy_static;
@@ -16,26 +17,45 @@ extern crate serde_derive;
 extern crate petgraph;
 extern crate serde_regex;
 extern crate toml;
+
+use clap::{App, Arg};
 use ignore::WalkBuilder;
 use parking_lot::{Mutex, RwLock};
 use petgraph::stable_graph::StableDiGraph;
 use petgraph::visit::Topo;
+use rayon::prelude::*;
 use std::collections::HashMap;
 use std::path::Path;
 use std::sync::mpsc::channel;
 use std::sync::Arc;
 mod rule;
 
-// use std::process::Command;
-// use petgraph::graphmap::DiGraphMap;
-
 lazy_static! {
+    static ref MATCHES: clap::ArgMatches<'static> = { clap_setup() };
     static ref RULES: std::collections::HashMap<std::string::String, rule::Rule> =
         rule::read_rules();
-    static ref FILES: Mutex<HashMap<Arc<String>, petgraph::prelude::NodeIndex>> =
-        Mutex::new(HashMap::new());
+    static ref FILES: RwLock<HashMap<Arc<String>, petgraph::prelude::NodeIndex>> =
+        RwLock::new(HashMap::new());
     static ref FILE_GRAPH: RwLock<StableDiGraph<Arc<String>, &'static rule::Rule>> =
         RwLock::new(StableDiGraph::new());
+}
+
+fn clap_setup() -> clap::ArgMatches<'static> {
+    App::new("make_ultra")
+        .arg(
+            Arg::with_name("dry_run")
+                .help("Print commands, but do not run them.")
+                .long("dry")
+                .short("n"),
+        )
+        .arg(
+            Arg::with_name("dot")
+                .help("Print dot graph to file.")
+                .long("dot")
+                .short("d")
+                .takes_value(true),
+        )
+        .get_matches()
 }
 
 fn main() {
@@ -45,7 +65,7 @@ fn main() {
         .standard_filters(false)
         .build_parallel()
         .run(move || {
-            let tx2 = tx.clone();
+            let tx = tx.clone();
             Box::new(move |entry| {
                 let entry = match entry {
                     Err(_e) => {
@@ -55,7 +75,7 @@ fn main() {
                 };
                 let path = entry.path().to_owned();
                 let p = String::from(path.to_string_lossy());
-                tx2.send(p).unwrap();
+                tx.send(p).unwrap();
                 ignore::WalkState::Continue
             })
         });
@@ -66,13 +86,13 @@ fn main() {
             });
         }
     });
-    {
+    if MATCHES.is_present("dot") {
         use petgraph::dot::{Config, Dot};
         use std::fs::File;
         use std::io::Write;
         let fg = FILE_GRAPH.read();
 
-        let mut file = File::create(Path::new("test.dot")).unwrap();
+        let mut file = File::create(Path::new(MATCHES.value_of("dot").unwrap())).unwrap();
         file.write_all(format!("{:?}", Dot::with_config(&*fg, &[Config::EdgeNoLabel])).as_bytes())
             .unwrap();
     }
@@ -83,19 +103,51 @@ fn main() {
             topo_visitor = Topo::new(&*g);
         }
         rayon::scope(move |s| {
+            use petgraph::visit::IntoNodeReferences;
             let g = FILE_GRAPH.read();
-            while let Some(node) = topo_visitor.next(&*g) {
+            for i in g.node_indices().filter(|n| {
+                // Get all nodes with no inputs (roots)
+                g.neighbors_directed(*n, petgraph::Direction::Incoming)
+                    .count()
+                    == 0
+            }) {
                 s.spawn(move |_| {
-                    let g = FILE_GRAPH.read();
-                    for edge in g.edges_directed(node, petgraph::Direction::Outgoing) {
-                        let command = (edge.weight().command.replace("$i", &*g[node]))
-                            .replace("$o", &*g[petgraph::visit::EdgeRef::target(&edge)]);
-                        println!("{}", command);
-                    }
+                    run_commands(i);
                 });
             }
         });
     }
+}
+
+fn run_commands(node: petgraph::prelude::NodeIndex) {
+    rayon::scope(move |s| {
+        use rayon::iter::ParallelBridge;
+        use std::process::Command;
+        let g = FILE_GRAPH.read();
+        g.edges_directed(node, petgraph::Direction::Outgoing)
+            .par_bridge()
+            .for_each(|edge| {
+                let g = FILE_GRAPH.read();
+                let files = FILES.read();
+                let full_command = (edge.weight().command.replace("$i", &*g[node]))
+                    .replace("$o", &*g[petgraph::visit::EdgeRef::target(&edge)]);
+                println!("{}", full_command);
+                if !MATCHES.is_present("dry_run") {
+                    let first_space = full_command.find(' ').unwrap();
+                    let command = &full_command[..first_space];
+                    let args: Vec<&str> = full_command[first_space + 1..].split(' ').collect();
+                    Command::new(command)
+                        .args(&args)
+                        .output()
+                        .expect("failed to execute process");
+                }
+                run_commands(
+                    *files
+                        .get(&*g[petgraph::visit::EdgeRef::target(&edge)])
+                        .unwrap(),
+                );
+            });
+    });
 }
 
 /// Adds `path` to the DAG if it was not already in it, then recursively adds
@@ -109,7 +161,7 @@ fn generate_children(path: String) {
                     let mut new_file: String;
                     let mut should_update_children = false;
                     {
-                        let mut files = FILES.lock();
+                        let mut files = FILES.write();
                         let node = if let Some(node_index) = files.get(path) {
                             *node_index
                         } else {
@@ -146,8 +198,6 @@ fn generate_children(path: String) {
         }
     });
 }
-
-fn run_commands(node: petgraph::prelude::NodeIndex) {}
 
 // fn make_children<'a>(
 //     file: Arc<file::File>,
